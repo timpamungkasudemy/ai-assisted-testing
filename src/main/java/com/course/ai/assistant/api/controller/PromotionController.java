0ab10913bf6c61e41b57404ea752fcec1556776d
timpamungkasudemy <timpamungkas.udemy@gmail.com>
2024-02-29 07:22:31 +0700

    Add generateArticle endpoint to PromotionController

--- src/main/java/com/course/ai/assistant/api/controller/PromotionController.java
+++ src/main/java/com/course/ai/assistant/api/controller/PromotionController.java
@@ -7,14 +7,21 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.course.ai.assistant.api.response.SimpleMessageResponse;
+import com.github.javafaker.Faker;
+
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @Tag(name = "Promotion")
 public class PromotionController {
+
+  private Faker faker = new Faker();
 
   @Operation(summary = "Redirect to promotion page")
   @GetMapping(path = "/api/promotion/fake/{promotion-code}")
@@ -30,6 +37,15 @@ public class PromotionController {
     return ResponseEntity
         .ok("<html><head><title>" + promotionCode + "</title></head><body><h2>Promotion details for: <strong>"
             + promotionCode + "</strong></h2></body></html>");
+  }
+
+  @Operation(summary = "Generate article")
+  @GetMapping(path = "/api/promotion/article", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<SimpleMessageResponse> generateArticle(
+      @RequestParam(name = "sentence-count", required = true) @Parameter(description = "Article size (in sentences)") int sentenceCount) {
+    var article = faker.lorem().paragraph(sentenceCount);
+
+    return ResponseEntity.ok(SimpleMessageResponse.builder().message(article).build());
   }
 
 }
